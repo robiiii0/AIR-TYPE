d17cce8f4195fc3567471ec620b7cbba446eb376
Anton Jourdheuil <anton.jourdheuil@epitech.eu>
2023-11-29 15:56:01 +0000

    [FIX] architecture game engine

---
+++ output/src/GameEngine/EntityManager/ComponentManager/GenericComponents/ULongComponent/ULongComponent.hpp
@@ -0,0 +1,9 @@
+namespace Engine {
+    namespace EntityManager {
+        namespace ComponentManager {
+            namespace GenericComponents {
+                typedef unsigned long ULongComponent;
+            }
+        }  // namespace ComponentManager
+    }      // namespace EntityManager
+};         // namespace Engine
