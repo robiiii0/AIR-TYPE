ab02cae3ef5bf5a3d376b192aee8f5b59a48d131
Valentin Woehrel <valentin.woehrel@epitech.eu>
2023-11-30 14:43:53 +0100

    [ADD] HmiModule class

---
+++ output/src/GameEngine/HmiModule/HmiModule.cpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2023
+** AIR-TYPE
+** File description:
+** HmiModule
+*/
+
+#include "HmiModule.hpp"
+
+Engine::Hmi::HmiModule::HmiModule() {}
+
+Engine::Hmi::HmiModule::~HmiModule() {}
---
+++ output/src/GameEngine/HmiModule/HmiModule.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2023
+** AIR-TYPE
+** File description:
+** HmiModule
+*/
+
+#ifndef HMIMODULE_HPP_
+#define HMIMODULE_HPP_
+
+namespace Engine {
+    namespace Hmi {
+        class HmiModule {
+            public:
+                HmiModule();
+                ~HmiModule();
+
+            protected:
+            private:
+        };
+    };  // namespace Hmi
+};      // namespace Engine
+
+#endif /* !HMIMODULE_HPP_ */
