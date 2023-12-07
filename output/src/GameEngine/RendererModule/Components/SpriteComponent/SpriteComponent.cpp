883836037618a9bb73a6b96b9609d5ef528b4c77
Anton Jourdheuil <anton.jourdheuil@epitech.eu>
2023-12-07 11:46:01 +0000

    [ADD] spriteComponen

--- output/src/GameEngine/RendererModule/Components/SpriteComponent/SpriteComponent.cpp
+++ output/src/GameEngine/RendererModule/Components/SpriteComponent/SpriteComponent.cpp
@@ -12,13 +12,17 @@ Engine::RendererModule::Components::SpriteComponent::SpriteComponent(
     std::uint32_t id, std::string component_name, std::any value)
 {
     _id = id;
-    _component_name = component_name;
+    _component_name = "SpriteComponent";
     if (value.type() != typeid(std::string))
         throw std::runtime_error("SpriteComponent: value is not a string");
     else {
 
         _path = std::any_cast<std::string>(value);
+        if (!_texture.loadFromFile(_path))
+            throw std::runtime_error("SpriteComponent: texture not found");
+        _sprite.setTexture(_texture);
         _ptr = value;
+        
     }
 }
 
@@ -40,3 +44,37 @@ std::string Engine::RendererModule::Components::SpriteComponent::getName() const
     return (_component_name);
 }
 
+void Engine::RendererModule::Components::SpriteComponent::setRotation(
+    float rotation)
+{
+    _sprite.setRotation(rotation);
+}
+
+void Engine::RendererModule::Components::SpriteComponent::setScale(float x,
+    float y)
+{
+    _sprite.setScale(x, y);
+}
+
+void Engine::RendererModule::Components::SpriteComponent::setOrigin(float x,
+    float y)
+{
+    _sprite.setOrigin(x, y);
+}
+
+void Engine::RendererModule::Components::SpriteComponent::setRepeated(
+    bool repeated)
+{
+    _texture.setRepeated(repeated);
+}
+
+void Engine::RendererModule::Components::SpriteComponent::setSmooth(bool smooth)
+{
+    _texture.setSmooth(smooth);
+}
+
+void Engine::RendererModule::Components::SpriteComponent::setPosition(float x,
+    float y)
+{
+    _sprite.setPosition(x, y);
+}
