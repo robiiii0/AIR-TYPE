b3fc008dc00450b1ba8c66dedf89a88e34363942
Eddie klaï <91609481+Eddieklai@users.noreply.github.com>
2023-12-07 13:00:58 +0100

    [ADD] management Window.cpp

---
+++ output/src/GameEngine/RendererModule/WindowManager/Window.cpp
@@ -0,0 +1,73 @@
+/*
+** EPITECH PROJECT, 2023
+** AIR-TYPE [Codespaces: studious memory]
+** File description:
+** WindowComponent
+*/
+
+#include "./Window.hpp"
+
+Engine::RendererModule::Window::Window() = default;
+
+Engine::RendererModule::Window::~Window() = default;
+
+void Engine::RendererModule::Window::setSize(sf::Vector2u size, sf::Window window)
+{
+    window.setSize(size);
+}
+
+void Engine::RendererModule::Window::setFramerateLimit(unsigned int limit, sf::Window window)
+{
+    window.setFramerateLimit(limit);
+}
+
+void Engine::RendererModule::Window::setVerticalSyncEnabled(bool enabled, sf::Window window)
+{
+    window.setVerticalSyncEnabled(enabled);
+}
+
+void Engine::RendererModule::Window::setTitle(std::string title, sf::Window window)
+{
+    window.setTitle(title);
+}
+
+sf::Vector2u Engine::RendererModule::Window::getSize(sf::Window window) const
+{
+    return (window.getSize());
+}
+
+bool Engine::RendererModule::Window::isFocus(sf::Window window) const
+{
+    return (window.hasFocus());
+}
+
+void Engine::RendererModule::Window::setIcon(sf::Image icon, sf::Window window)
+{
+    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+}
+
+void Engine::RendererModule::Window::setMouseCursorVisible(bool visible, sf::Window window)
+{
+    window.setMouseCursorVisible(visible);
+}
+
+void Engine::RendererModule::Window::setMouseCursorGrabbed(bool grabbed, sf::Window window)
+{
+    window.setMouseCursorGrabbed(grabbed);
+}
+
+void Engine::RendererModule::Window::setKeyRepeatEnabled(bool enabled, sf::Window window)
+{
+    window.setKeyRepeatEnabled(enabled);
+}
+
+void Engine::RendererModule::Window::requestFocus(sf::Window window)
+{
+    window.requestFocus();
+}
+
+sf::WindowHandle Engine::RendererModule::Window::getSystemHandle(sf::Window window) const
+{
+    return (window.getSystemHandle());
+}
+
---
+++ output/src/GameEngine/RendererModule/WindowManager/Window.hpp
@@ -0,0 +1,42 @@
+/*
+** EPITECH PROJECT, 2023
+** AIR-TYPE [Codespaces: studious memory]
+** File description:
+** WindowComponent
+*/
+
+#ifndef WINDOWCOMPONENT_HPP_
+#define WINDOWCOMPONENT_HPP_
+
+#include "../../EntityManager/ComponentManager/IComponent/IComponent.hpp"
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+namespace Engine {
+    namespace RendererModule {
+        class Window {
+            public:
+                Window();
+                ~Window();
+
+                void setSize(sf::Vector2u size, sf::Window window);
+                void setFramerateLimit(unsigned int limit, sf::Window window);
+                void setVerticalSyncEnabled(bool enabled, sf::Window window);
+                void setTitle(std::string title, sf::Window window);
+                void setIcon(sf::Image icon, sf::Window window);
+                void setMouseCursorVisible(bool visible, sf::Window window);
+                void setMouseCursorGrabbed(bool grabbed, sf::Window window);
+                void setKeyRepeatEnabled(bool enabled, sf::Window window);
+                void requestFocus(sf::Window window);
+
+                sf::Vector2u getSize(sf::Window window) const;
+                bool         isFocus(sf::Window window) const;
+                sf::WindowHandle getSystemHandle(sf::Window window) const;
+
+            protected:
+            private:
+        };
+    };  // namespace RendererModule
+};      // namespace Engine
+
+#endif /* !WINDOWCOMPONENT_HPP_ */
