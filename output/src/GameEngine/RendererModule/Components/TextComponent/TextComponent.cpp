297bd19d8d2633620bfdbe66fb04ae851033d049
AymericChaverot <aymeric.chaverot@gmail.com>
2023-12-11 14:56:02 +0100

    [ADD] TextComponent

---
+++ output/src/GameEngine/RendererModule/Components/TextComponent/TextComponent.cpp
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2023
+** output
+** File description:
+** TextComponent
+*/
+
+#include "TextComponent.hpp"
+
+template<typename T>
+Engine::RendererModule::Components::TextComponent<T>::TextComponent(T value) {
+    if (typeid(T) != typeid(TextData))
+        throw std::runtime_error("TextComponent: value is not a TextData");
+    _data = value;
+    _text.setString(_data.text);
+    _text.setFont(_data.font);
+    _text.setColor(_data.color);
+    _text.setPosition(_data.position);
+    _text.setScale(_data.scale);
+    _text.setRotation(_data.rotation);
+}
+
+template<typename T>
+Engine::RendererModule::Components::TextComponent<T>::~TextComponent() {}
+
+template<typename T>
+void Engine::RendererModule::Components::TextComponent<T>::execute() {}
+
+template<typename T>
+std::string Engine::RendererModule::Components::TextComponent<T>::getName() const {
+    return "RendererTextComponent";
+}
---
+++ output/src/GameEngine/RendererModule/Components/TextComponent/TextComponent.hpp
@@ -0,0 +1,44 @@
+/*
+** EPITECH PROJECT, 2023
+** output
+** File description:
+** TextComponent
+*/
+
+#ifndef TEXTCOMPONENT_HPP_
+#define TEXTCOMPONENT_HPP_
+
+#include <SFML/Graphics.hpp>
+
+#include "../../../EntityManager/ComponentManager/AComponent/AComponent.hpp"
+
+namespace Engine {
+    namespace RendererModule {
+        namespace Components {
+            struct TextData {
+                    std::string  text;
+                    sf::Font    &font;
+                    sf::Color    color;
+                    sf::Vector2f position;
+                    sf::Vector2f scale;
+                    float        rotation;
+            };
+
+            template<typename T>
+            class TextComponent : public Engine::Entity::Component::AComponent {
+                public:
+                    TextComponent(T value);
+                    ~TextComponent();
+                    void        execute() override;
+                    std::string getName() const override;
+
+                protected:
+                private:
+                    T        _data;
+                    sf::Text _text;
+            };
+        }  // namespace Components
+    }      // namespace RendererModule
+}  // namespace Engine
+
+#endif /* !TEXTCOMPONENT_HPP_ */
