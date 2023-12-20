4d245fc5fe37f3857bc8b2e3194af7ee7009d626
anton jourdheuil <anton.jourdheuil@epitech.eu>
2023-12-20 15:32:11 +0100

    [ADD] exception for entityManager

---
+++ output/src/GameEngine/EntityManager/exceptions/CouldNotCreateEntity/CouldNotCreateEntity.cpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2023
+** AIR-TYPE
+** File description:
+** CouldNotCreateEntity
+*/
+
+#include "CouldNotCreateEntity.hpp"
+
+Engine::EntityManager::CouldNotCreateEntity::CouldNotCreateEntity() {}
+
+Engine::EntityManager::CouldNotCreateEntity::~CouldNotCreateEntity() {}
+
+const char *Engine::EntityManager::CouldNotCreateEntity::what()
+    const noexcept {
+    return "Could not create entity";
+}
