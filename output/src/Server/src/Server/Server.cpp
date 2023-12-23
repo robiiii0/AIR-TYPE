34c12681641f8518164dfccf182b5939a2071dba
AymericChaverot <aymeric.chaverot@gmail.com>
2023-12-23 22:44:26 +0100

    [ADD] very basic architecture of server

---
+++ output/src/Server/src/Server/Server.cpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2023
+** output
+** File description:
+** Server
+*/
+
+#include "Server.hpp"
+
+Server::Server() {}
+
+Server::~Server() {}
+
+int Server::run()
+{
+    return 0;
+}
---
+++ output/src/Server/src/Server/Server.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2023
+** output
+** File description:
+** Server
+*/
+
+#ifndef SERVER_HPP_
+#define SERVER_HPP_
+
+class Server {
+    public:
+        Server();
+        ~Server();
+
+        int run();
+
+    protected:
+    private:
+};
+
+#endif /* !SERVER_HPP_ */
