1015b8a16d2f4fa981a41044cf11a82f5f956942
AymericChaverot <aymeric.chaverot@gmail.com>
2023-12-23 23:09:04 +0100

    [ADD] server base, does not work because networking is not multithreaded correctly

--- output/src/Server/src/Server/Server.cpp
+++ output/src/Server/src/Server/Server.cpp
@@ -7,11 +7,28 @@
 
 #include "Server.hpp"
 
-Server::Server() {}
+Server::Server() { init(); }
 
 Server::~Server() {}
 
-int Server::run()
-{
+int Server::run() {
+    _running = true;
+    _networkingModule->run();
+    while (_running) {
+        loop();
+    }
     return 0;
 }
+
+void Server::init() {
+    _running = false;
+    _networkingModule = std::make_shared<Engine::Network::NetworkingModule>(
+        4242, Engine::Network::NetworkingTypeEnum::UDP, 10);
+}
+
+void Server::loop() {
+    std::cout << "loop" << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+}
+
+void Server::stop() { _running = false; }
--- output/src/Server/src/Server/Server.hpp
+++ output/src/Server/src/Server/Server.hpp
@@ -8,6 +8,8 @@
 #ifndef SERVER_HPP_
 #define SERVER_HPP_
 
+#include "../../../GameEngine/NetworkingModule/NetworkingModule.hpp"
+
 class Server {
     public:
         Server();
@@ -16,7 +18,13 @@ class Server {
         int run();
 
     protected:
+        void init();
+        void loop();
+        void stop();
+
     private:
+        bool _running;
+        std::shared_ptr<Engine::Network::NetworkingModule> _networkingModule;
 };
 
 #endif /* !SERVER_HPP_ */
