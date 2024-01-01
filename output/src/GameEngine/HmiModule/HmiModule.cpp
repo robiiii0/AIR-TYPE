3178282e85ebbb213f9fd784859becd158960fb4
Valentin Woehrel <91880965+Valent1W@users.noreply.github.com>
2024-01-01 23:16:40 +0100

    [ADD] keyEvent function in HmiModule class

--- output/src/GameEngine/HmiModule/HmiModule.cpp
+++ output/src/GameEngine/HmiModule/HmiModule.cpp
@@ -5,6 +5,35 @@
 #include "HmiModule.hpp"
 
 Engine::HmiModule::HmiModule() {
-    _data.id = 0;
+    _data.keyboard.reset();
     std::cout << "HmiModule created" << std::endl;
+}
+
+Engine::HmiData &Engine::HmiModule::getData() {
+    return _data;
+}
+
+void Engine::HmiModule::keyEvent(sf::Event &event) {
+    switch (event.key.code) {
+        case sf::Keyboard::Up:
+            _data.keyboard.set(0);
+            break;
+        case sf::Keyboard::Down:
+            _data.keyboard.set(1);
+            break;
+        case sf::Keyboard::Left:
+            _data.keyboard.set(2);
+            break;
+        case sf::Keyboard::Right:
+            _data.keyboard.set(3);
+            break;
+        case sf::Keyboard::Space:
+            _data.keyboard.set(4);
+            break;
+        case sf::Keyboard::Escape:
+            _data.keyboard.set(5);
+            break;
+        default:
+            break;
+    }
 }
--- output/src/GameEngine/HmiModule/HmiModule.hpp
+++ output/src/GameEngine/HmiModule/HmiModule.hpp
@@ -6,12 +6,14 @@
 #define NETWORKING_HMIMODULE_HPP
 
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 #include <iostream>
+#include <bitset>
 
 namespace Engine {
     struct HmiData {
-        int id;
+        std::bitset<8> keyboard;
     };
 
         class HmiModule {
@@ -19,7 +21,16 @@ namespace Engine {
                 HmiModule();
                 ~HmiModule() = default;
 
+                HmiData &getData();
+
+                void keyEvent(sf::Event &event);
+
             private:
+            /**
+                 * @brief up = 0, down = 1, left = 2, right = 3, space = 4,
+                 * escape = 5
+                 *
+            */
             HmiData _data{};
         };
 }  // namespace Engine
