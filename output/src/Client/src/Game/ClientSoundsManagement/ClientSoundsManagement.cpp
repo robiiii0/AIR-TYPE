7ebaf8f4e5ec712597e2c25717d44f3be55240c5
Valentin Woehrel <91880965+Valent1W@users.noreply.github.com>
2024-01-04 14:41:02 +0100

    [ADD] LoadSound() function in client management

---
+++ output/src/Client/src/Game/ClientSoundsManagement/ClientSoundsManagement.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** AIR-TYPE
+** File description:
+** ClientSoundsManagement
+*/
+
+#include "../Client.hpp"
+
+void Client::LoadSound(Engine::RendererModule::Components::SoundData &data) {
+    std::shared_ptr<Engine::RendererModule::Components::SoundComponent>
+        soundComponent = std::make_shared<
+            Engine::RendererModule::Components::SoundComponent>(data);
+
+    _sounds.push_back(soundComponent->get());
+}
