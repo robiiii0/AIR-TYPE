1841beadce185552aeec7379c9ccb1ff1a4085d3
Eddie klaï <91609481+Eddieklai@users.noreply.github.com>
2024-01-10 18:47:58 +0100

    [FIX] Format

--- output/src/AdvancedTrack/src/Game/GameManagement/GameManagement.cpp
+++ output/src/AdvancedTrack/src/Game/GameManagement/GameManagement.cpp
@@ -9,8 +9,8 @@
 
 void Game::setGame() {
     createSprite({0.0, 0.0}, {3, 2.5}, _textures[0], "");
-    createButton(std::bind(&Game::changeGameState, this, GameState::MENU), "Go Back",
-                 _textures[1], _fonts[0],
+    createButton(std::bind(&Game::changeGameState, this, GameState::MENU),
+                 "Go Back", _textures[1], _fonts[0],
                  {static_cast<float>(_screenWidth / 1.05),
                   static_cast<float>(_screenHeight / 1.05)},
                  {0.3, 0.3}, sf::Color::White, 0);
