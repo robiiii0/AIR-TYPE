121b54972e5aab9a1c0222d6c236a7d43ccabf0b
Valentin Woehrel <valentin.woehrel@epitech.eu>
2024-01-10 14:02:28 +0100

    [ADD] Game system functions

--- output/src/AdvancedTrack/src/Game/Game.cpp
+++ output/src/AdvancedTrack/src/Game/Game.cpp
@@ -8,10 +8,27 @@
 #include "Game.hpp"
 
 Game::Game() : _gameEngine() {
+    _screenWidth = sf::VideoMode::getDesktopMode().width > 1920 ? 1920 : 1920;
+    _screenHeight = sf::VideoMode::getDesktopMode().height > 1080 ? 1080 : 1080;
+    _gameEngine.getRendererModule()->init(_screenWidth, _screenHeight,
+                                          "PokeHunter", 60);
+
+    _hmiModule = std::make_shared<Engine::HmiModule>();
     _gameState = GameState::MENU;
 }
 
 void Game::run() {
+    setUpState();
+    while (_gameEngine.getRendererModule()->getWindow().isOpen()) {
+        _gameEngine.getRendererModule()->update(
+            *_gameEngine.getEntityManager(), getEntities());
+        std::string eventKey = _hmiModule->keyEvent(
+        _gameEngine.getRendererModule()->UpdateForServer(
+            *_gameEngine.getEntityManager(), getEntities()));
+
+        _gameEngine.getRendererModule()->render(*_gameEngine.getEntityManager(),
+                                    getEntities());
+    }
 }
 
 void Game::handleExit() {
