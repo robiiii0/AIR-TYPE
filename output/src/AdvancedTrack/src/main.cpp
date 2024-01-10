ac6c0e408c0b969453ef645c0f4b0d3ac76be588
Valentin Woehrel <valentin.woehrel@epitech.eu>
2024-01-10 10:38:05 +0100

    [ADD] main structure for advanced track game

---
+++ output/src/AdvancedTrack/src/main.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** AIR-TYPE
+** File description:
+** main
+*/
+
+#include <iostream>
+
+#include "../../GameEngine/GameEngine.hpp"
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
