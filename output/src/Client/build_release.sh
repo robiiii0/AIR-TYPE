3e0f6c944cf96b4783578938d6046cad78e20864
AymericChaverot <aymeric.chaverot@gmail.com>
2024-01-11 17:03:13 +0100

    [ADD] release directory and fix typo in build_release.sh

--- output/src/Client/build_release.sh
+++ output/src/Client/build_release.sh
@@ -2,11 +2,13 @@
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+mkdir "$CURRENT_DIR/release"
+
 cmake -B "$CURRENT_DIR/release" -S "$CURRENT_DIR" "-DCMAKE_TOOLCHAIN_FILE=$CURRENT_DIR/../../../vcpkg/scripts/buildsystems/vcpkg.cmake" -DCMAKE_BUILD_TYPE=Release
 
 cmake --build "$CURRENT_DIR/release"
 
-if [ ! -e "$CURRENT_DIR/release/r-type_client" ] && [ ! -e "$CURRENT_DIR/build/Debug/r-type_client.exe" ]; then
+if [ ! -e "$CURRENT_DIR/release/r-type_client" ] && [ ! -e "$CURRENT_DIR/release/r-type_client.exe" ]; then
   echo "Client binary is missing"
   exit 1
 else
